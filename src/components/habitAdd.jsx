e2abd5999d73e546b76bcee659dc2fc8974ee121
Muntaeyeon <monty420420@naver.com>
2022-01-05 23:53:44 +0900

    habitAdd_input2

--- src/components/habitAdd.jsx
+++ src/components/habitAdd.jsx
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 
 
 class HabitAdd extends Component {
-
-
+    inputRef = React.createRef(); //React는 가상dom을 사용하여 dom을 직접사용하지않기 때문에 ref이용
+  onSubmit = event => {
+      event.preventDefault(); //submit이 실행되면 리로드되는데 리로드를 막기위한 함수
+      console.log(this.inputRef.current.value);
+  }
   render() {
+      
     return(
-        <form className='add-form'>
-            <input type="text" className='add-input' placeholder='Habit' />
+        <form className='add-form' onSubmit={this.onSubmit}>
+            <input ref={this.inputRef} type="text" className='add-input' placeholder='Habit' />
             <button className='add-button'>Add</button>
         </form>
     );
