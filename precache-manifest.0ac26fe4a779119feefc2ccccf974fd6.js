fba3bb513b3f0178ca7f6c4adb61d35438c3d7b2
Muntaeyeon <monty420420@naver.com>
2022-01-12 22:54:33 +0900

    Updates

---
+++ precache-manifest.0ac26fe4a779119feefc2ccccf974fd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb982d8713ea2d4302b87fc8739bf54f",
+    "url": "/React_template/index.html"
+  },
+  {
+    "revision": "3b24291ca162d624acff",
+    "url": "/React_template/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "88af2cb585e2c7ef5d80",
+    "url": "/React_template/static/js/2.4ed1eb19.chunk.js"
+  },
+  {
+    "revision": "3b24291ca162d624acff",
+    "url": "/React_template/static/js/main.6db924dc.chunk.js"
+  },
+  {
+    "revision": "1b3ff86955a16fd30451",
+    "url": "/React_template/static/js/runtime~main.a9ccce07.js"
+  }
+]);
