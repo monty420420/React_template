53c1d5207f8d8df4cf4999934b8615613d2151dc
Muntaeyeon <monty420420@naver.com>
2022-01-12 22:15:17 +0900

    react_Hook3

--- src/components/simpleHabit.jsx
+++ src/components/simpleHabit.jsx
@@ -2,15 +2,15 @@ import React, { useState, useRef, useCallback, useEffect } from 'react';
 
 const SimpleHabit = () => {
   const [count, setCount] = useState(0); //[count라는변수, count를 업데이트할수있는함수]
-  const spanRef = useRef();
+  const spanRef = useRef(); //createRef와는다르게 한번만 만들고 재사용가능하도록 메모리에 저장
 
-  const handleIncrement = useCallback(() => {
+  const handleIncrement = useCallback(() => {  //usecallback은 콜백함수를 재사용가능하도록 메모리에 저장
     setCount(count + 1);
   });
 
   useEffect(() => {
-    console.log(`mounted & updated!: ${count}`);
-  }, []);
+    console.log(`mounted & updated!: ${count}`);}, []); //[]넣으면 처음에만 호출
+ // console.log(`mounted & updated!: ${count}`);}, [count]);  count가 변경될때마다 띄우기
   return (
     <li className="habit">
       <span ref={spanRef} className="habit-name">Reading</span>
