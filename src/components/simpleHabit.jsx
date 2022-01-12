65437bf80ad6a1a310de53dbdf3078407469f149
Muntaeyeon <monty420420@naver.com>
2022-01-12 21:21:03 +0900

    react_hook2

--- src/components/simpleHabit.jsx
+++ src/components/simpleHabit.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useCallback, useEffect } from 'react';
 
 const SimpleHabit = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); //[count라는변수, count를 업데이트할수있는함수]
   const spanRef = useRef();
 
   const handleIncrement = useCallback(() => {
@@ -13,9 +13,7 @@ const SimpleHabit = () => {
   }, []);
   return (
     <li className="habit">
-      <span ref={spanRef} className="habit-name">
-        Reading
-      </span>
+      <span ref={spanRef} className="habit-name">Reading</span>
       <span className="habit-count">{count}</span>
       <button className="habit-button habit-increase" onClick={handleIncrement}>
         <i className="fas fa-plus-square"></i>
